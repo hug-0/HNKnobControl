eeef8e1b8b2b3d32695f6c45376d30d16de6510b
hug-0 <hugo.nordell@gmail.com>
2014-10-25 15:17:40 +0200

    Initial Commit

---
+++ HNKnobControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HNKnobControl
+//
+//  Created by Hugo Nordell on 10/25/14.
+//  Copyright (c) 2014 hugo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
